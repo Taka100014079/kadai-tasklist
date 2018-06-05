f9f70055aeb336e286c309a1761a3ca56d5bb2a9
takaya.hirano <takaya.hirano@rakuten.com>
2018-06-05 01:49:32 +0000

    add column

---
+++ resources/views/tasklists/show.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>id = {{ $tasklist->id }} のタスクの詳細ページ</h1>
+
+    <p>ステータス: {{ $tasklist->status }}</p>
+    <p>メッセージ: {{ $tasklist->content }}</p>
+    
+     {!! link_to_route('tasklists.edit', 'このタスクを編集', ['id' => $tasklist->id]) !!}
+
+
+{!! Form::model($tasklist, ['route' => ['tasklists.destroy', $tasklist->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除') !!}
+    {!! Form::close() !!}
+
+
+@endsection
